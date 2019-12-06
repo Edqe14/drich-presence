10ea716b7e0ab13f5af5e0b57c09cdf4dfb19c36
Edqe <34704796+Edqe14@users.noreply.github.com>
2019-12-06 18:05:58 +0700

    Update richPresence.js

--- main/scripts/richPresence.js
+++ main/scripts/richPresence.js
@@ -1,7 +1,8 @@
 const RPC = require("discord-rpc");
-let client;
+let client = null;
 
 function set() {
+	if(client) client.destroy();
     const id = document.getElementById('client'),
           state = document.getElementById('state'),
           details = document.getElementById('details'),
@@ -11,26 +12,27 @@ function set() {
           smallImageText = document.getElementById('smallText');
 
     if(!id || id.value.length <= 0) return;
+	
+	client = new RPC.Client({ transport: 'ipc' });
+	client.login({ clientId: id.value });
 
-    client = new RPC.Client({ transport: 'ipc' });
-    client.login({ clientId: id.value });
-    
-    client.once('ready', ()=>{
-        let presence = {
-            instance: true
-        };
-        if(state && state.value.length > 0) presence.state = state.value;
-        if(details && details.value.length > 0) presence.details = details.value;
-        if(largeImageKey && largeImageKey.value.length > 0) presence.largeImageKey = largeImageKey.value;
-        if(smallImageKey && smallImageKey.value.length > 0) presence.smallImageKey = smallImageKey.value;
-        if(largeImageText && largeImageText.value.length > 0) presence.largeImageText = largeImageText.value;
-        if(smallImageText && smallImageText.value.length > 0) presence.smallImageText = smallImageText.value;
+	client.once('ready', () => {
+		let presence = {
+			instance: true
+		};
+		if (state && state.value.length > 0) presence.state = state.value;
+		if (details && details.value.length > 0) presence.details = details.value;
+		if (largeImageKey && largeImageKey.value.length > 0) presence.largeImageKey = largeImageKey.value;
+		if (smallImageKey && smallImageKey.value.length > 0) presence.smallImageKey = smallImageKey.value;
+		if (largeImageText && largeImageText.value.length > 0) presence.largeImageText = largeImageText.value;
+		if (smallImageText && smallImageText.value.length > 0) presence.smallImageText = smallImageText.value;
 
-        client.setActivity(presence);
-    });
+		client.setActivity(presence);
+	});
 };
 
 function clear() {
-    if(!client) return;
+    //if(!client) return;
     client.clearActivity();
+	client = null;
 }
